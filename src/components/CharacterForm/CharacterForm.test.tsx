f824f87f4378d26624534afacaff6856e26193af
myspecialspace <myspecialspace@users.noreply.github.com>
2023-03-26 20:40:19 +0300

    feat: add test to CharacterForm

--- src/components/CharacterForm/CharacterForm.test.tsx
+++ src/components/CharacterForm/CharacterForm.test.tsx
@@ -1,24 +1,34 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Character from './CharacterForm';
-// import characters from '../../helpers/data/potter.json';
+import CharacterForm from './CharacterForm';
 
-describe('Character', () => {
-  // const data = characters[0];
-
+describe('CharacterForm', () => {
   it('Renders!', () => {
-    const component = render(<Character />);
+    const component = render(<CharacterForm />);
     expect(component.container).not.toBeEmptyDOMElement();
   });
 
-  // it('name render', () => {
-  //   render(<Character className="character" />);
-
-  //   expect(screen.getByRole('heading', { level: 2 })).toBeDefined();
-  // });
-
-  it('some fields render', () => {
-    render(<Character />);
-    expect(screen.getByText('Wizard')).toBeDefined();
+  it('Notification', () => {
+    const component = render(<CharacterForm />);
+    expect(component.queryByText(/Изменения сохранены/i)).toBeNull();
   });
+
+  it('form controls render', () => {
+    render(<CharacterForm />);
+    expect(screen.getByRole('textbox', { name: '' })).toBeDefined();
+    expect(screen.getByRole('radio', { name: 'Male' })).toBeDefined();
+    expect(screen.getByRole('radio', { name: 'Female' })).toBeDefined();
+    expect(screen.getByRole('option', { name: 'human' })).toBeDefined();
+    expect(screen.getByRole('option', { name: 'Gryffindor' })).toBeDefined();
+    expect(screen.getByRole('button')).toBeDefined();
+  });
+
+  // it('submit empty form has validation error', () => {
+  //   const component = render(<CharacterForm />);
+
+  //   const button = component.getByText(/submit/i);
+  //   fireEvent.click(button);
+
+  //   expect(screen.getByText(/Обязательное поле/)).toBeDefined();
+  // });
 });
