2a0ef259aea5e9360e46ac179a8eaac54d4f9a27
myspecialspace <myspecialspace@users.noreply.github.com>
2023-04-04 22:59:59 +0300

    refactor: CardList and SearchBar

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -4,7 +4,7 @@ import styles from './CardList.module.scss';
 
 interface Props {
   characters: Character[];
-  onClick?: (character: Character) => unknown;
+  onClick: (character: Character) => unknown;
 }
 
 function CardList({ onClick, characters }: Props): JSX.Element {
@@ -12,15 +12,11 @@ function CardList({ onClick, characters }: Props): JSX.Element {
     <ul className={styles.cards}>
       {characters.map((character) => (
         <li className={styles.card} key={character.id}>
-          <Card data={character} className="card" onClick={() => onClick?.(character)} />
+          <Card data={character} className="card" onClick={() => onClick(character)} />
         </li>
       ))}
     </ul>
   );
 }
-
-CardList.defaultProps = {
-  onClick: () => {},
-};
 
 export default CardList;
