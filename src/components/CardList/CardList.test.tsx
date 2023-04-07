56988e4aee62e816dcb0ef590b0b77a81321b226
myspecialspace <myspecialspace@users.noreply.github.com>
2023-04-07 18:30:04 +0300

    refactor: CardList and CardList test

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,17 +1,16 @@
 import { describe, it } from 'vitest';
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import CardList from './CardList';
 import characters from '../../mocks/characters';
-import { customRender } from '../../test/utils';
 
 describe('CardList', () => {
   it('Renders!', () => {
-    const component = customRender(<CardList characters={[]} onClick={() => {}} />);
+    const component = render(<CardList characters={[]} onClick={() => {}} />);
     expect(component.container).not.toBeEmptyDOMElement();
   });
 
   it('Renders 5 list item!', () => {
-    customRender(<CardList characters={characters.data.slice(0, 5)} onClick={() => {}} />);
+    render(<CardList characters={characters.data.slice(0, 5)} onClick={() => {}} />);
     expect(screen.getAllByRole('listitem')).toHaveLength(5);
   });
 });
