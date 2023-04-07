114597ee8b07bc873e416e50291e7907a0c8be78
myspecialspace <myspecialspace@users.noreply.github.com>
2023-04-07 18:20:59 +0300

    refactor: refactor App.test.tsx, tsconfig.json, utils.ts and index.html

--- src/components/CharacterForm/utils.ts
+++ src/components/CharacterForm/utils.ts
@@ -1,4 +1,6 @@
 import { Character, CharacterAttrs } from '../../types/character';
+import formObj from './form';
+import { FormControlValue, FormControlType, FormErrors, ControlValidatorError } from './types';
 
 export const getBase64Image = (file: File): Promise<string> => {
   return new Promise((resolve) => {
@@ -8,6 +10,68 @@ export const getBase64Image = (file: File): Promise<string> => {
     };
     reader.readAsDataURL(file);
   });
+};
+
+export const getFormValue = async (form: HTMLFormElement): Promise<Partial<CharacterAttrs>> => {
+  const formControls = form!.elements;
+
+  const acc: Partial<CharacterAttrs> = {};
+
+  for (let i = 0; i < formObj.length; i += 1) {
+    const control = formObj[i];
+    const controlElement = formControls.namedItem(control.name)!;
+
+    let value: FormControlValue;
+
+    switch (control.type) {
+      case FormControlType.TEXT:
+      case FormControlType.RADIO:
+        value = (controlElement as HTMLInputElement).value;
+        break;
+
+      case FormControlType.DATE:
+        value = (controlElement as HTMLInputElement).value;
+        break;
+
+      case FormControlType.SELECT:
+        value = (controlElement as HTMLSelectElement).value;
+        break;
+
+      case FormControlType.IMAGE: {
+        const file = (controlElement as HTMLInputElement).files?.[0];
+        value = file ? await getBase64Image(file) : '';
+        break;
+      }
+
+      default:
+        throw Error(`Uknown control type "${control.type}".`);
+    }
+
+    (acc[control.name] as FormControlValue) = value;
+  }
+
+  return acc;
+};
+
+export const getFormErrors = (formValue: Partial<CharacterAttrs>): FormErrors => {
+  return formObj.reduce((acc, control) => {
+    const value = formValue[control.name];
+
+    const errors: ControlValidatorError[] = [];
+
+    control.validators.forEach((fn) => {
+      const result = fn(value!);
+      if (result) {
+        errors.push(result);
+      }
+    });
+
+    if (errors.length > 0) {
+      acc[control.name] = errors;
+    }
+
+    return acc;
+  }, {} as FormErrors);
 };
 
 const DEFAULT_CHARACTER_ATTR: CharacterAttrs = {
