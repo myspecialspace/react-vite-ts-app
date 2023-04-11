17887648854014fd38d2c1990e6245efc8466d6f
myspecialspace <myspecialspace@users.noreply.github.com>
2023-04-11 09:56:38 +0300

    refactor: validator tests and utils in CharacterForm

--- src/components/CharacterForm/validators.test.tsx
+++ src/components/CharacterForm/validators.test.tsx
@@ -6,24 +6,10 @@ describe('validators', () => {
     const result = validators.required('');
     expect(result).toEqual('Required field');
   });
-  it('required "example"', () => {
-    const result = validators.required('example');
-    expect(result).toEqual('');
-  });
-
-  it('minLength 5 "example"', () => {
-    const result = validators.minLength(5)('example');
-    expect(result).toEqual(' ');
-  });
 
   it('minLength 5 "hi"', () => {
     const result = validators.minLength(5)('hi');
     expect(result).toEqual('Minimum length 5 symbols');
-  });
-
-  it('date past', () => {
-    const result = validators.date('Sun Mar 26 2023 19:05:06 GMT+0300 (Москва, стандартное время)');
-    expect(result).toEqual('');
   });
 
   it('date future', () => {
