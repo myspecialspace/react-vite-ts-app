f872bedf70d1f0922844bedfc5d5f3e103783ce3
myspecialspace <myspecialspace@users.noreply.github.com>
2023-04-17 00:10:46 +0300

    refactor: change CharacterForm

--- src/components/CharacterForm/types.ts
+++ src/components/CharacterForm/types.ts
@@ -3,8 +3,8 @@ import { CharacterAttrs } from '../../types/character';
 export type FormValue = Partial<CharacterAttrs>;
 export type FormControlValue = CharacterAttrs[FormControlName];
 
-export type ControlValidator = (value?: string | string[] | undefined) => ControlValidatorError;
-export type ControlValidatorError = string | string[] | undefined;
+export type ControlValidator = (value?: string) => ControlValidatorError;
+export type ControlValidatorError = string | undefined;
 
 export type FormErrors = Record<FormControlName, ControlValidatorError[]>;
 
