625ece89a44e7188e7375fb84fe9304b8e48f9f3
myspecialspace <myspecialspace@users.noreply.github.com>
2023-04-16 23:25:21 +0300

    refactor: delete search.ts with LocalStorage, change Main.tsx in pages, add status.ts

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -3,41 +3,42 @@ import { useSelector } from 'react-redux';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import styles from './Main.module.scss';
 import CardList from '../../components/CardList/CardList';
-import { Character } from '../../types/character';
-import Status from '../../types/status';
 import Modal from '../../components/Modal/Modal';
 import CharacterModal from '../../components/CharacterModal/CharacterModal';
 import Loader from '../../components/Loader/Loader';
-import { getSearchItem } from '../../helpers/search';
+import { characterModalSelectors, mainSelectors } from '../../store/selectors';
+import { useAppDispatch } from '../../store';
+import { fetchCharacters } from '../../store/thunks/main';
+import { getStatus } from '../../helpers/status';
+import { characterModalActions } from '../../store/slices/character-modal';
 
 export default function MainPage() {
-  const [search, setSearch] = useState(getSearchItem());
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [error, setError] = useState<string>('');
-  const [status, setStatus] = useState<Status>(Status.INITIAL);
-  const [characterModal, setCharacterModal] = useState<Character | null>();
+  const { search, characters, error, status } = useSelector(mainSelectors.self);
+  const characterModal = useSelector(characterModalSelectors.character);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(fetchCharacters(search));
   }, [search, dispatch]);
 
-  const isLoading = status === Status.PENDING;
-  const isSuccess = status === Status.SUCCESS;
-  const isError = status === Status.ERROR;
+  const { isError, isLoading, isSuccess } = getStatus(status);
   const isEmpty = isSuccess && characters.length === 0;
 
   return (
     <div className={styles.root}>
-      <SearchBar className="search" onChange={(value) => setSearch(value)} />
+      <SearchBar />
       {isLoading && <Loader />}
       {isError && <div className={styles.error}>An error has occurred: {error}</div>}
       {isEmpty && (
         <div className={styles.empty}>Nothing was found, try to change your search parameters.</div>
       )}
       {isSuccess && (
-        <CardList characters={characters} onClick={(character) => setCharacterModal(character)} />
+        <CardList
+          characters={characters}
+          onClick={(character) => dispatch(characterModalActions.set(character))}
+        />
       )}
-      <Modal isOpen={!!characterModal} onClose={() => setCharacterModal(null)}>
+      <Modal isOpen={!!characterModal} onClose={() => dispatch(characterModalActions.reset())}>
         <CharacterModal character={characterModal!} />
       </Modal>
     </div>
